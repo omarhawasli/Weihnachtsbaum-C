fd4b625cc19c70210a50afbc17a0e19d95216dd8
omarhawasli <amr.hawasli.1@gmail.com>
2023-07-04 00:13:33 +0200

    first commit

---
+++ weihnachtsbaum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int i=0,j=0,k=0;
+	
+		for(i=2;i<20;i++){
+				
+			for(j=1;j<i;j++){
+				printf("*");
+			}
+			for(k=1;k<i;k++){
+			printf("*");
+				}
+			
+	  printf("\n");
+		}
+	return 0;
+}
